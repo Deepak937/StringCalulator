be975df838e7c55ef00b717386d4b81f73c31499
Deepak Tripathi <46919636+Deepak937@users.noreply.github.com>
2020-09-19 01:55:06 +0530

    Create CalculatorTest.java

---
+++ CalculatorTest.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+	@Test
+	public void shouldReturnZeroOnEmptyString() {
+		assertEquals(0,Calculator.add(""));
+	}	
+}
