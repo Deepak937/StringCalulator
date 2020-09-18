bef05e06177fc61d0b944233cb6ba07c15ba510c
Deepak Tripathi <46919636+Deepak937@users.noreply.github.com>
2020-09-19 01:59:50 +0530

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -8,5 +8,10 @@ public class CalculatorTest {
 	@Test
 	public void shouldReturnZeroOnEmptyString() {
 		assertEquals(0,Calculator.add(""));
-	}	
+	}
+	
+	@Test
+	public void shouldReturnNumberOnNumber() {
+		assertEquals(1,Calculator.add("1"));
+	}
 }
